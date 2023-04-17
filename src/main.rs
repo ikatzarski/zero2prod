d608d9500e1662d6149540054772f4082f5938e5
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2023-04-17 23:31:19 +0100

    Add a DB connection pool and test against a real Postgres container

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration, startup};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Panic if we can't read configuration
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    startup::run(listener)?.await
+    startup::run(listener, connection_pool)?.await
 }
