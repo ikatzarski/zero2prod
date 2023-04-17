ca54350a4324a355dd880022666f6966c5074f30
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2023-04-17 19:39:34 +0100

    Split the project into sub-modules

---
+++ src/startup.rs
@@ -0,0 +1,14 @@
+use crate::routes::{health_check, subscriptions};
+use actix_web::{dev::Server, web, App, HttpServer};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check::health_check))
+            .route("/subscriptions", web::post().to(subscriptions::subscribe))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
